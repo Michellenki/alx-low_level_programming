4cda7858767721aa4ccdc720073d122b26a9016a
Michellenki <gitongamichelle7@gmail.com>
2023-12-14 11:48:26 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main this is the main function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+printf("%s, but the outcome is a piece of art,\n", "with proper grammar");
+return (0);
+}
